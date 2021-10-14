479c8767da3aaa8594fe248ddab115d3dd0b5643
Sylvester Arikhan <arikhansylvester@gmail.com>
2021-10-15 00:07:38 +0200

    Cloud Firestore

--- components/Chat.js
+++ components/Chat.js
@@ -4,13 +4,19 @@ import
   View, 
   StyleSheet, 
   Platform, 
-  KeyboardAvoidingView 
+  KeyboardAvoidingView,
+  LogBox  
 } from 'react-native';
 
 import 
 { GiftedChat, 
-  Bubble 
+  Bubble,
+  Button
 } from 'react-native-gifted-chat';
+
+//Firebase Database
+const firebase = require('firebase');
+require('firebase/firestore');
 
 
 export default class Chat extends React.Component {
@@ -18,36 +24,102 @@ export default class Chat extends React.Component {
     super();
     this.state = {
       messages: [],
+      uid: 0,
+      loggedInText: "Please wait, you are getting logged in",
     }
+    // Firebase Config
+    const firebaseConfig ={
+      apiKey: "AIzaSyCjhBRSiv-Y-AjqCv70pD1Hpd_iDClsFH0",
+      authDomain: "chatapp-fff4f.firebaseapp.com",
+      projectId: "chatapp-fff4f",
+      storageBucket: "chatapp-fff4f.appspot.com",
+      messagingSenderId: "470458640572",
+      appId: "1:470458640572:web:dc189ccc2d71f27e5148b9"
+    }
+
+    // Connect to Firebase
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+    // References Firebase messages
+    this.referenceChatMessages = firebase.firestore().collection('messages');
   }
 
-  componentDidMount() {
-    //get the name from the home screen and change the title of the page to the name of the user
-    let name = this.props.route.params.name;
-    this.props.navigation.setOptions({ title: name });
-
-    //set a static message to start as a test
+  // Updating messages state
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // go through each document
+    querySnapshot.forEach((doc) => {
+      // get the QueryDocumentSnapshot's data
+      let data = doc.data();
+      messages.push({
+        _id: data._id,
+        createdAt: data.createdAt.toDate(),
+        text: data.text || '',
+        user: {
+          _id: data.user._id,
+          name: data.user.name,
+        },
+        image: data.image,
+        location: data.location
+      });
+    });
     this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: `Hello, ${name}!`,
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-        {
-          _id: 2,
-          text: `${name} has entered the chat`,
-          createdAt: new Date(),
-          system: true,
-        },
-      ],
-    })
+      messages,
+    });
   }
+
+  // Adding messages to cloud storage
+  addMessage() {
+    const message = this.state.messages[0];
+    this.referenceChatMessages.add({
+      _id: message._id,
+      uid: this.state.uid,
+      createdAt: message.createdAt,
+      text: message.text || '',
+      user: message.user,
+    });
+  }
+  //when a user clicks Send, attach the new message to the message status object
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }),
+  )
+}
+
+componentDidMount() {
+  //get name from start screen and change title of page to user's name
+  let name = this.props.route.params.name;
+  //get the name from the home screen and change the title of the page to the name of the user
+  this.props.navigation.setOptions({ title: name });
+
+  // listen to authentication events
+  this.authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
+    if (!user) {
+      firebase.auth().signInAnonymously();
+    }
+
+  //update user state with currently active user data
+  this.setState({
+    uid: user.uid,
+    loggedInText: 'Hello there',
+  });
+
+  this.unsubscribe = this.referenceChatMessages
+     .orderBy("createdAt", "desc")
+     .onSnapshot(this.onCollectionUpdate);
+  // Create reference to the active users messages
+    this.referenceChatMessages = firebase.firestore().collection('messages').where("uid", "==", this.state.uid);
+  });
+}
+
+componentWillUnmount() {
+  // stop listening to authentication
+    this.authUnsubscribe();
+    // stop listening for changes
+    this.unsubscribe(); 
+}
 
   //when a user clicks Send, attach the new message to the message status object
   onSend(messages = []) {
@@ -56,19 +128,19 @@ export default class Chat extends React.Component {
     }));
   }
 
-  //Change the color of the right bubble (of the sender
-  renderBubble(props) {
-    return (
-      <Bubble
-        {...props}
-        wrapperStyle={{
-          right: {
-            backgroundColor: '#000'
-          }
-        }}
-      />
-    )
-  }
+//Change the color of the right bubble (of the sender
+renderBubble(props) {
+  return (
+    <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: '#000'
+        }
+      }}
+    />
+  )
+} 
 
   render() {
     //get the name and background color that the user selected on the home screen to change the
@@ -81,29 +153,35 @@ export default class Chat extends React.Component {
           <GiftedChat
             //change the color of the chat bubble
             renderBubble={this.renderBubble.bind(this)}
+            renderUsernameOnMessage={true}
+            renderCustomView={this.renderCustomView}
+            renderActions={this.renderActions}
             messages={this.state.messages}
             onSend={messages => this.onSend(messages)}
             user={{
               _id: 1,
             }}
-          />
+          />  
           {Platform.OS === 'android' ? 
           <KeyboardAvoidingView behavior="height" /> : null}
         </View>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    overflow: 'hidden',
     justifyContent: 'center',
+    alignItems: 'center',
     alignItems: 'center'
   },
   giftedChat: {
     flex: 1,
     width: '88%',
-    paddingBottom: 10
+    paddingBottom: 10,
+    justifyContent: 'center'
   }
 });
